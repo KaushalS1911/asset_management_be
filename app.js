8b2f9ebf757e1c2647a5fc02932d7eaa798136d9
KaushalS19 <kaushalsojitra19@gmail.com>
2024-08-30 12:26:04 +0530

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser')
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ mongoose.connect(process.env.DB_CONNECTION_STRING)
 
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
